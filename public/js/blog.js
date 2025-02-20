fc0f1d5273b73a3837eb16c202e261786588217d
Błażej <wargun364@gmail.com>
2025-02-20 20:30:59 +0100

    logic

---
+++ public/js/blog.js
@@ -0,0 +1,16 @@
+
+$('.delete-blog').on('click',function(){
+    const blogId=$(this).data('id');
+    if(confirm('Are you sure you want to delete this blog?')){
+        axios.delete(`/blog/${blogId}`)
+            .then(response => {
+                console.log('🗑️ Blog deleted:', response.data);
+                window.location.href='/';
+            })
+            .catch(error => {
+                console.error('❌ Error deleting blog:', error)
+                window.location.reload();
+            });
+    }
+    
+})
--- src/index.js
+++ src/index.js
@@ -2,14 +2,71 @@ import express from 'express';
 
 const app=express();
 const port = 3000;
+let blogs = []
 
 app.use(express.static('../public'));
 app.use(express.urlencoded({extended: true}));
 
+const BlogCreator= (()=>{  
+    let nextId = 0
+    return class BlogCreator{
+        constructor(design, title, content){
+            this.id= nextId++;
+            this.design= design;
+            this.title= title;
+            this.content= content;
+        }
+    };
+})();
+
 app.get('/', (req, res)=>{
-    res.render('home.ejs')
+    console.log(blogs)
+    res.render('home.ejs',{blogs});
+})
+
+app.get('/create-blog', (req, res)=>{
+    res.render('createBlog.ejs');
+
+})
+app.post('/create-blog', (req, res)=>{
+    const data = req.body;
+    console.log(data)
+    const blog = new BlogCreator(data.design, data.title, data.content);
+    blogs.push(blog);
+    res.redirect(`/blog/${blog.id}`)
+
+})
+app.get(`/blog/:id`, (req, res)=>{
+    const blogId = parseInt(req.params.id,10)
+    const blog = blogs.find((e)=>e.id===blogId);
+
+    if(blog){
+        res.render('blog.ejs',{blog});
+    }
+    else{
+        res.status(404).render('error.ejs');
+    }
+
+})
+
+app.put(`/blog/:id`, (req, res)=>{
+    const blogId = parseInt(req.params.id,10)
+    res.render(`/blog/${blogId}`);
+})
+
+app.delete(`/blog/:id`, (req, res)=>{
+    const blogId = parseInt(req.params.id,10);
+    const beforeLength = blogs.length;
+    blogs = blogs.filter(e => e.id !== blogId);
+
+    if (blogs.length < beforeLength) {
+        res.status(200).json({ message: `🗑️ Blog ${blogId} deleted.` });
+    } else {
+        res.status(404).json({ error: '❌ Blog not found.' });
+    }
 })
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}.`);
 })
+
