a1493f1bf2c2c12e81ff83f5315c4c8918f0f5fc
Błażej <wargun364@gmail.com>
2025-03-03 23:00:01 +0100

    create

--- public/js/default.js
+++ public/js/default.js
@@ -7,6 +7,51 @@ $('.dropdown-toggle').on('click',()=>{
 })
 
 $(document).ready(function() {
+    let inputFile = $('input[type="file"]')
+    let label = $('#file-content')
+    let prevLabel = label.html();
+
+    inputFile.on('change', function(e){
+        let fileName = '';
+        if(e.target.value){
+            fileName=e.target.value.split('\\').pop();
+
+            label.addClass('ms-4');
+            $('.file-name-wrap .bi-check').removeClass('d-none').addClass('d-inline');
+        }
+        else{
+            label.removeClass('ms-4');
+            $('.file-name-wrap .bi-check').removeClass('d-inline').addClass('d-none');
+        }
+        !fileName? label.html(prevLabel): fileName.length >45?label.html(fileName.slice(0,45)+"..."): label.html(fileName);
+    })
+
+    $('.list-group-item').each(function(){
+        let item = $(this);
+        let checkLast = item.children("span").text().trim() === "Design";
+        let designText = 'modern'
+
+        if(!checkLast){
+            item.on('click',function(){
+                let prev = $('.list-group-item').filter('.bg-light');
+
+                if($(this).attr('class') !== prev.attr('class')){
+                    prev.removeClass("bg-light").addClass('lh-condensed hover-effect')
+                    prev.find('.text-success').removeClass('text-success').children('small').addClass('text-muted')
+
+                    $(this).addClass("bg-light").removeClass('lh-condensed hover-effect')
+                    $(this).children('div').addClass('text-success').children('small').removeClass('text-muted')
+                    
+                    designText = prev.children('div').find('h6').text();
+                    item.children("strong").text(designText)
+                }
+            })
+        }
+        else{
+            
+        }
+    })
+
     if(window.innerWidth<768){
         $('.home-img').addClass('img-fluid');
 
